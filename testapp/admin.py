6c48da4dc3c04ce5886adb73c7e678a1e267f764
blaise-m <mubangiziblaise@gmail.com>
2021-04-08 20:58:25 +0300

    Add a test model

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from testapp.models import TestModel
 
-# Register your models here.
+
+admin.site.register(TestModel)
--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    firstname = models.CharField(max_length=120)
+    lastname = models.CharField(max_length=120)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
